8dc64072ed9e4e95700a2207e693614682dd91a4
Matt Robertson <mattrobertson333@gmail.com>
2021-05-21 20:00:08 -0500

    Added Hilt

--- app/src/main/java/com/mattrobertson/wcf/WCFApplication.kt
+++ app/src/main/java/com/mattrobertson/wcf/WCFApplication.kt
@@ -3,8 +3,10 @@ package com.mattrobertson.wcf
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
 @SuppressLint("StaticFieldLeak")
+@HiltAndroidApp
 class WCFApplication: Application() {
 
     override fun onCreate() {
